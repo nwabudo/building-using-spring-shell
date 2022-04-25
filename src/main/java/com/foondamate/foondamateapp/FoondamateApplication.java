87736d724912ef5c9fc7771eafc9a32d8e37e8d2
neoOkpara <emmox55@gmail.com>
2022-04-25 09:09:34 +0100

    removed the use of RestTemplate and replaced with FeignClient impl

--- src/main/java/com/foondamate/foondamateapp/FoondamateApplication.java
+++ src/main/java/com/foondamate/foondamateapp/FoondamateApplication.java
@@ -4,10 +4,12 @@ import com.foondamate.foondamateapp.helpers.ShellHelper;
 import org.jline.terminal.Terminal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FoondamateApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/foondamate/foondamateapp/client/DataClient.java
+++ src/main/java/com/foondamate/foondamateapp/client/DataClient.java
@@ -1,8 +1,14 @@
 package com.foondamate.foondamateapp.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.Map;
 
+@FeignClient(name = "data-service", url = "${data.service.url}")
 public interface DataClient {
 
+    @GetMapping("")
     Map<String, Integer> fetchApiData();
 }
+
